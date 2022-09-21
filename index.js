a04358515cccb194ed2b51c9237bf3ede7ad6af5
paullaster <paullasterokoth98@gmail.com>
2022-09-21 16:42:24 +0300

    converting to minute

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ class Employee {
     }
     get name () { return this.fname + ' ' + this.lname; };
     get lengthOfService () {
-        
-    }
+        /**
+         * if 1 second = 1000 milliseconds,
+         * :. ?       = yearsOfExperience
+         * if 60 seconds = 1 minute,
+         * :. yearsOfExperience(s) = ?
+         */
+        let yearsOfExperience = Math.abs(new Date () - new Date (this.datajoined));
+        return Math.ceil((yearsOfExperience / 1000) / 3600);
+    };
 };
 
-console.log(typeof '2022-09-20')
+let emp1 = new Employee ('Paullaster', 'Okoth', '2020-10-01');
+
+console.log(emp1.lengthOfService);
+
