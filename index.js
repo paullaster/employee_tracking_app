8ed7f99a1511064e13f89f6e4399c5fa55bfa215
paullaster <paullasterokoth98@gmail.com>
2022-09-21 16:49:33 +0300

    making data private

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
  * Employee records App
  */
 class Employee {
+    #fname;
+    #lname;
+    #datajoined;
     constructor (fname, lname, datajoined) {
-        this.fname = fname;
-        this.lname = lname;
-        this.datajoined = datajoined;
+        this.#fname = fname;
+        this.#lname = lname;
+        this.#datajoined = datajoined;
     }
-    get name () { return this.fname + ' ' + this.lname; };
+    get name () { return this.#fname + ' ' + this.#lname; };
     get lengthOfService () {
         /**
          * if 1 second = 1000 milliseconds,
@@ -15,9 +18,12 @@ class Employee {
          * if 60 seconds = 1 minute,
          * :. yearsOfExperience(s) = ?
          */
-        let yearsOfExperience = Math.abs(new Date () - new Date (this.datajoined));
+        let yearsOfExperience = Math.abs(new Date () - new Date (this.#datajoined));
         return Math.ceil(((yearsOfExperience / 1000) / 3600)/ 8784);
     };
+    getEmployeeDetails () {
+
+    }
 };
 
 let emp1 = new Employee ('Paullaster', 'Okoth', '2020-10-01');
